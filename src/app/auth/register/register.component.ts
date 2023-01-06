4ff603dd4281f07251ef0a5c423c28767e841814
cubewebs <juanjoseaos@hotmail.com>
2023-01-06 20:06:42 +0000

    Registro validaciones creadas

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,6 +7,36 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+
+	registroForm: FormGroup;
+
+	constructor(public fb: FormBuilder) {
+		this.registroForm = this.fb.group({
+			nombre: ['', Validators.required],
+			correo: ['', Validators.required],
+			password: ['', Validators.required],
+		})
+	}
+	
+	ngOnInit(): void {
+
+	}
+
+	get getNombre() {
+	return this.registroForm.get('nombre')
+	}
+
+	get getCorreo() {
+	return this.registroForm.get('correo')
+	}
+
+	get getPassword() {
+	return this.registroForm.get('password')
+	}
+
+	registrarUsuario() {
+		console.log('this.registroForm.value ->', this.registroForm.value)
+	}
 
 }
